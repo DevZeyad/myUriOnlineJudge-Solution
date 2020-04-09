ad854327b1c201a6aeef2003a524eef2c456180b
Zeyad Fouad Salem Elsaied <62177326+DevZeyad@users.noreply.github.com>
2020-04-09 18:52:03 +0200

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,7 @@
+NUMBER = int(input())
+workedHoursInMonth = int(input())
+amountPerHour = float(input())
+SALARY = workedHoursInMonth * amountPerHour
+
+print("NUMBER =",NUMBER,end='\n')
+print("SALARY = U$",'%.2f'%SALARY,end='\n')
